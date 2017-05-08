bab3298b9146b31e75784a20652da932f8411805
Kevin Simon <syasnomis93@gmail.com>
2017-05-07 18:39:39 -0700

    Add first instruction: ADC Immediate

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -2,17 +2,36 @@
 #include <iostream>
 
 Cpu::Cpu() {
-  P = 0x34;
-  A = X = Y = 0;
-  S = 0xFD;
+  P = 0x34; 
+  A = X = Y = 0; 
+  S = 0xFD; 
+
+  // todo: ensure rom from cart is in mem to ensure correct init of PC
+  
+  // pc reset vector
+  PC = (mem[0xFFFD] << 8) + mem[0xFFFC];
 }
 
+
 void Cpu::fdxCycle() {
-  switch (PC) {
-    case 0x1:
+  uint8_t opcode = mem[PC];
+  switch (opcode) {
+
+    // ADC Immediate
+    case 0x69:
+      {
+        auto C = P & 0x1;
+        auto nn = mem[PC+1];
+        P &= 0xFE;
+        P |= (((A+C+nn) & 0x100) >> 8);
+        A = A+C+nn;
+      }
+      PC += 2;
       break;
+
     default:
-      std::cout << "UNIMPLEMTNED OPCODE: " << PC << std::endl;
+      std::cout << "UNIMPLEMENTED OPCODE: " << std::hex << opcode << std::endl;
+      std::cout << "PC: " << PC;
       while(1) {
         // loop forever
       }
--- src/cpu.h
+++ src/cpu.h
@@ -1,25 +1,25 @@
 #include <cstdint>
 
+#define CPU_MEM_SIZE 65536
+
+/**
+ * Data structure emulating NES CPU core (mos 6502)
+ * Register mapping:
+ * A = Accumulator
+ * X, Y = General purpose registers
+ * S = Stack pointer
+ * P = Status register
+ * PC = Program counter
+ */
+
 struct Cpu {
   uint8_t A, X, Y, S, P; 
   uint16_t PC; 
+  uint8_t mem[CPU_MEM_SIZE];
 
   Cpu();
 
   // fetch, decode, execute cycle
   void fdxCycle();
 
-  // opcodes named by instruction mnemonics
-  void ADC();
-  void AND();
-  void ASL();
-  void BIT();
-  void BPL();
-  void BMI();
-  void BVC();
-  void BCC();
-  void BCS();
-  void BNE();
-  void BEQ();
-  void BRK();
 };
